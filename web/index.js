b49c508258df1c6a504b2d2e3f9ab48cb92a6278
Nivid Limbasiya <nlimbasiya24@gmail.com>
2022-11-13 11:00:20 +0530

    testing

--- web/index.js
+++ web/index.js
@@ -147,7 +147,7 @@ export async function createServer(
     try {
       await productCreator(session);
     } catch (e) {
-      console.log(`Failed to process Please change it products/create: ${e.message}`);
+      console.log(`Failed to process products/create: ${e.message}`);
       status = 500;
       error = e.message;
     }
